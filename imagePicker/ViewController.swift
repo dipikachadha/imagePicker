7e638293dd1844eeb4269e3c0589ea9521dd846d
Dipika Chadha <dipika.chadha@icloud.com>
2019-07-02 22:05:37 -0400

    Added activity and alert view presentation code

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -19,5 +19,25 @@ class ViewController: UIViewController {
         let nextController = UIImagePickerController()
         present(nextController, animated: true, completion: nil)
     }
+    
+    @IBAction func experimentActivity(_ sender: Any) {
+        
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentAlert(_ sender: Any) {
+        
+        let controller = UIAlertController()
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default) { action in self.dismiss(animated: true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
+        self.present(controller, animated: true, completion: nil)
+    }
 }
 
