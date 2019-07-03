ae45fc1fffd028248cf14b4a612fe3fd19f340dd
Dipika Chadha <dipika.chadha@icloud.com>
2019-07-02 21:02:37 -0400

    Initial commit, project completed!

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func experiment() {
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
+    }
 }
 
